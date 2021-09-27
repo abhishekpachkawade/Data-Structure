4c4b6adbe030f4b7a4e2dc309ab4aa7606fed986
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-09-27 22:25:19 +0530

    Added: Delete last element using PopUp method

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -66,4 +66,24 @@ public class MyLinkedListTest {
 		boolean result = myLinkedList.head.equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
 		Assert.assertTrue(result);
 	}
+
+	@Test
+
+	public void givenLastElementDeletedShouldPassLinkedListResult() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.append(myFirstNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThirdNode);
+		myLinkedList.printMyNodes();
+		myLinkedList.popLast();
+		myLinkedList.printMyNodes();
+		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
+				&& myLinkedList.tail.equals(mySecondNode);
+		Assert.assertTrue(result);
+	}
+
+	
 }
